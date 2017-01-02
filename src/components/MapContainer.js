f796dd33fd7ec31bc137793ec3c387ab4bd0d9fc
askaper <andrew@skaper.info>
2017-01-02 12:36:09 -0600

    Changed component names for MapContainer Component

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import Map from 'google-maps-react';
 
-class JobMap extends Component {
+class MapContainer extends Component {
 
   render() {
     return(
       <div className="map-container">
         <div className="map">
           This is/will be the map itself.
-          <Map google={window.google}></Map>
+          <Map google={this.props.google}></Map>
         </div>
 
         <div className="map-marker-details">
@@ -19,4 +19,4 @@ class JobMap extends Component {
   }
 }
 
-export default JobMap;
+export default MapContainer;
