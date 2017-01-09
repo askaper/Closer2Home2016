26915ce9b715f1fc3ee300acc8d4a2d67f0e2dc4
askaper <andrew@skaper.info>
2017-01-09 10:21:34 -0600

    Integrated Google API Key into component

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -9,6 +9,10 @@ class MapContainer extends Component {
         <div className="map">
           This is/will be the map itself.
           <GoogleMap
+            bootstrapURLKeys={{
+              key: 'AIzaSyCFTQw147or_eOBf4FY7bQliAuHcuVQSZc',
+              language: 'en'
+            }}
             classname={'map'}
             center={[59.744465, 30.042834]}
             zoom={10}>
