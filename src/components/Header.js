b1984986bd6cac85b50088723e26481d0e85ef56
askaper <andrew@skaper.info>
2017-01-11 09:25:06 -0600

    Improved naming on div container for bottom part of header, cleaned up margin with media query

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,7 +19,7 @@ class Header extends Component {
           </div>
         </div>
         <div className="header-bottom">
-          <div className="header-slogan-text">
+          <div className="header-slogan-text-areas">
             <p className="slogan">An easier commute makes a work of difference...</p>
             <p className="header-tagline">Find a job, post a job, let the neighbors know!</p>
           </div>
