e3f9b9d0faf57d807989c1f9e60f207090f804f7
askaper <andrew@skaper.info>
2017-01-13 09:33:49 -0600

    Made Header component functional because stateless and y knot?

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Header extends Component {
-  render() {
-    return (
-      <div>
-        <div className="header-top">
-          <div className="header-top-logo">
-            <p className="logo">Closer 2 Home</p>
-          </div>
-          <div className="header-top-nav-items">
-            <ul className="links">
-              <li>Home</li>
-              <li>Submit a Job</li>
-              <li>Find a Job</li>
-              <li>FAQ</li>
-              <li>Contact</li>
-            </ul>
-          </div>
+const Header = props => {
+  return (
+    <div>
+      <div className="header-top">
+        <div className="header-top-logo">
+          <p className="logo">Closer 2 Home</p>
         </div>
-        <div className="header-bottom">
-          <div className="header-slogan-text-areas">
-            <p className="slogan">An easier commute makes a work of difference...</p>
-            <p className="header-tagline">Find a job, post a job, let the neighbors know!</p>
-          </div>
-          <div className="address-lookup-form">
-            <div className="address-lookup">
-              <form action="">
-                <input type="text" placeholder="Enter Your Address"/>
-                <button type="submit">Take a Closer Look</button>
-              </form>
-            </div>
+        <div className="header-top-nav-items">
+          <ul className="links">
+            <li>Home</li>
+            <li>Submit a Job</li>
+            <li>Find a Job</li>
+            <li>FAQ</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </div>
+      <div className="header-bottom">
+        <div className="header-slogan-text-areas">
+          <p className="slogan">An easier commute makes a work of difference...</p>
+          <p className="header-tagline">Find a job, post a job, let the neighbors know!</p>
+        </div>
+        <div className="address-lookup-form">
+          <div className="address-lookup">
+            <form action="">
+              <input type="text" placeholder="Enter Your Address"/>
+              <button type="submit">Take a Closer Look</button>
+            </form>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Header;
